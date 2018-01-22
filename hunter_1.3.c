e6ca2e46e5ee704bdfed3ab930d2971130f53711
vikas goyal <35465321+vikasgooyal@users.noreply.github.com>
2018-01-23 00:16:15 +0530

    Update hunter_1.3.c

--- hunter_1.3.c
+++ hunter_1.3.c
@@ -1,11 +1,4 @@
-#include <math.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
-
 int main() {
     int n,i,j,a[100],b[100],k,f=0,p=0; 
     scanf("%d", &n);
